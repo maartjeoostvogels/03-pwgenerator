a496d95e55eeefde5e99caf00fc0a8dc043d5d55
M. Oostvogels <moostvogels@hotmail.com>
2022-10-16 13:46:43 +0100

    Prompt user for password length and generate password

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,25 @@
-// Assignment code here
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
 
+function generatePassword() {
+  var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+  // Prompt for password length
+  var defaultPasswordLength = 8;
+  var passwordLength = prompt('What length password?  Please enter a number between 8 and 128', defaultPasswordLength);
+  if (passwordLength < 8 || passwordLength > 128) {
+    return 'Invalid, password length should be between 8 and 128'
+  }
+
+  // Generate a lowercase password
+  var password = '';
+  for (var i = 0; i < passwordLength; i++) {
+    password = password + lowerCase[getRandomInt(0,lowerCase.length)];
+  }
+
+  return password;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -8,6 +28,7 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
 
   passwordText.value = password;
 
