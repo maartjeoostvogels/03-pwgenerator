9fba22bc01a5230d185648f546d62c3cbe046b46
M. Oostvogels <moostvogels@hotmail.com>
2022-10-16 15:42:31 +0100

    Added character type arrays and added prompts

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,11 @@ function getRandomInt(min, max) {
 }
 
 function generatePassword() {
+  var characters =[]
   var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  var numbers = ["1","2","3","4","5","6","7","8","9","0"];
+  var special = ["!","£","$","%","^","&","*"];
 
   // Prompt for password length
   var defaultPasswordLength = 8;
@@ -12,10 +16,32 @@ function generatePassword() {
     return 'Invalid, password length should be between 8 and 128'
   }
 
-  // Generate a lowercase password
+  var useLowerCase = confirm("Would you like the password to include lowercase characters?");
+  var useUpperCase = confirm("Would you like the password to include uppercase characters?");
+  var useNumbers = confirm("Would you like the password to include numbers?");
+  var useSpecialCharacters = confirm("Would you like the password to include special characters?");
+
+  // Build up array of characters based on desired character types
+  if (useLowerCase) {
+    characters = characters.concat(lowerCase);
+  }
+
+  if (useUpperCase) {
+    characters = characters.concat(upperCase);
+  }
+
+  if (useNumbers) {
+    characters = characters.concat(numbers);
+  }
+
+  if (useSpecialCharacters) {
+    characters = characters.concat(special);
+  }
+
+  // Generate a password
   var password = '';
   for (var i = 0; i < passwordLength; i++) {
-    password = password + lowerCase[getRandomInt(0,lowerCase.length)];
+    password = password + characters[getRandomInt(0,characters.length)];
   }
 
   return password;
